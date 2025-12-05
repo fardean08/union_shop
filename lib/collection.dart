e34dd4e912a2a20657d64c845611471c0000b32e
fardean aymagh <fardeanaymagh22@gmail.com>
2025-12-05 00:54:36 +0000

    Added Collection model with name, imageUrl, and product list

---
+++ lib/collection.dart
@@ -0,0 +1,11 @@
+class Collection {
+  final String name;
+  final String imageUrl;
+  final List<Map<String, dynamic>> products;
+
+  Collection({
+    required this.name,
+    required this.imageUrl,
+    required this.products,
+  });
+}
