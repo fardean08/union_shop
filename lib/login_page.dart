85d383b845676b43dc8e3b6e3c1958eb0bb02dc3
fardean aymagh <fardeanaymagh22@gmail.com>
2025-12-05 00:26:20 +0000

    Enhance LoginPage: add SnackBar feedback for Sign Up button press

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -64,6 +64,24 @@ class LoginPage extends StatelessWidget {
                           style: TextStyle(
                               fontSize: 18, fontWeight: FontWeight.bold)),
                     ),
+                    ElevatedButton(
+                      onPressed: () {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Sign Up button pressed')),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFF4d2963),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 48, vertical: 16),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(6),
+                        ),
+                      ),
+                      child: const Text('Sign Up',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold)),
+                    ),
                     TextButton(
                       onPressed: () {
                         ScaffoldMessenger.of(context).showSnackBar(
